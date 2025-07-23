7edbf5ac102f948e52defc7156fee5ff10d12113
ezeksuresh03 <ezeksuresh03@gmail.com>
2025-07-24 03:50:39 +0800

    added getXPath function

--- src/utils/xpath.ts
+++ src/utils/xpath.ts
@@ -1,4 +1,30 @@
 export function getXPath(element: Element): string {
-    if(!element) return "nothing"
-    return "something"
+
+    let el: Element | null = element
+    if(!el) return "no xpath detected"
+
+    const parts: string[] = []
+    
+    while (el && !el.id){
+        let part = el.nodeName.toLowerCase()
+
+        if (el.parentNode){
+            const siblings = Array.from(el.parentNode.children).filter(
+                sibling => sibling.nodeName == el?.nodeName
+            )
+            if (siblings.length > 1){
+                const index = siblings.indexOf(el) + 1
+                part += `[${index}]`
+            }
+        }
+
+        parts.unshift(part)
+        el = el.parentElement
+    }
+
+    if (el && el.id){
+        return `//*[@id="${el.id}"]/${parts.join('/')}`
+    } else {
+        return "/" + parts.join("/")
+    } 
 }
